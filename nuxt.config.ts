ccf59e9908d8c524526b47459dc58cbf0ff0f648
aries <44047715+ariesaviles@users.noreply.github.com>
2024-09-06 00:23:45 -0400

    nuxt3 tailwind setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ["@/assets/styles/tailwind.css"],
+  modules: ["@nuxtjs/tailwindcss"],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./app.vue",
-    "./pages/**/*.{vue,js,ts}",
-    "./components/**/*.{vue,js,ts}",
-    "./layouts/**/*.{vue,js,ts}",
-    "./plugins/**/*.{js,ts}",
-    "./composables/**/*.{js,ts}",
-    "./error.vue",
-  ],
+  content: [],
   theme: {
+    fontFamily: {
+      sans: ['"Times New Roman"', "serif"],
+    },
     extend: {},
   },
   plugins: [],
