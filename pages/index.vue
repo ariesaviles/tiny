b6ab6eaad17e90ae369f4a94bb6bf17c1e92a35a
aries <44047715+ariesaviles@users.noreply.github.com>
2024-09-09 16:13:23 -0400

    font

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="p-6">
-    <h2 class="text-2xl font-bold mb-4">Welcome to the Homepage!</h2>
-    <p class="mb-4">This is the index page of your Nuxt app.</p>
-
+  <div class="flex flex-col p-6 space-y-4">
+    <h2 class="text-4xl font-bold mb-4">Welcome Home</h2>
+    <p class="mb-4">
+      Welcome to My Blog: Exploring the World of Code and Creativity.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac
+      ipsum et quam vestibulum consectetur non in libero. Vivamus eget leo non
+      nisi egestas luctus. Phasellus sit amet risus et orci fermentum suscipit.
+      Donec vehicula ante id tortor pulvinar, nec viverra turpis gravida. Nullam
+      venenatis, nisl sed viverra bibendum, sapien odio consectetur lectus, vel
+      sodales nisi enim ac lorem. Nulla vel vestibulum est.
+    </p>
     <NuxtLink to="/blog/01">
       <button
-        class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        class="pixelated px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
-        Go to Blog Post 01
+        01
       </button>
     </NuxtLink>
   </div>
