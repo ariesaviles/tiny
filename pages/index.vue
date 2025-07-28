0702970246f6ed746a911e46d598fa36d63bc08c
aries <ariesaviles.dev@gmail.com>
2025-07-28 13:09:45 -0400

    anonymize

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="p-6">
-    <h2 class="text-2xl font-bold mb-4">Welcome to the Homepage!</h2>
-    <p class="mb-4">This is the index page of your Nuxt app.</p>
+    <h2 class="text-2xl font-bold mb-4">Hey! Why are you here? 🤔</h2>
 
-    <NuxtLink to="/blog/01">
+    <!-- <NuxtLink to="/blog/01">
       <button
         class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
         Go to Blog Post 01
       </button>
-    </NuxtLink>
+    </NuxtLink> -->
   </div>
 </template>
